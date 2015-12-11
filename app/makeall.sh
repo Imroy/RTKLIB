77c61b3b48d9dc7ce7fabca71e6644c0ba91730a
Ian Tester <imroykun@gmail.com>
2015-12-11 16:15:27 +1100

    Simplify makeall.sh

--- app/makeall.sh
+++ app/makeall.sh
@@ -1,30 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 #
 # make all cui applications by gcc
 #
 
-echo; echo % pos2kml/gcc
-cd pos2kml/gcc
-make $1
-cd ../..
-
-echo; echo % str2str/gcc
-cd str2str/gcc
-make $1
-cd ../..
-
-echo; echo % rnx2rtkp/gcc
-cd rnx2rtkp/gcc
-make $1
-cd ../..
-
-echo; echo % convbin/gcc
-cd convbin/gcc
-make $1
-cd ../..
-
-echo; echo % rtkrcv/gcc
-cd rtkrcv/gcc
-make $1
-cd ../..
-
+for prog in pos2kml str2str rnx2rtkp convbin rtkrcv
+do
+  echo $prog
+  pushd `pwd`
+  cd $prog/gcc
+  make "$@" || exit
+  popd
+  echo
+done
